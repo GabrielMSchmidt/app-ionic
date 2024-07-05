f0327446e67b5f94a0bc74820e287873f3f5d241
GabrielMSchmidt <gabrielmarochischmidt@gmail.com>
2024-07-05 11:31:43 -0300

    Aula 05/07

---
+++ src/app/view/cadastrar/cadastrar.page.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
+import Contato from 'src/app/model/entities/Contato';
+import { ContatoService } from 'src/app/model/services/contato.service';
+
+@Component({
+  selector: 'app-cadastrar',
+  templateUrl: './cadastrar.page.html',
+  styleUrls: ['./cadastrar.page.scss'],
+})
+export class CadastrarPage implements OnInit {
+  nome: string;
+  telefone: string;
+  email: string;
+  genero: number;
+
+  constructor(private alertController: AlertController,
+    private contatoService: ContatoService,
+    private router: Router) {}
+
+  cadastrar() {
+    if (this.nome && this.telefone){
+      let c: Contato = new Contato(this.nome, this.telefone);
+      if (this.email){
+        c.email = this.email;
+      }
+      if (c.genero){
+        c.genero = this.genero;
+      }else{
+        c.genero = 0;
+      }
+      this.contatoService.cadastrar(c);
+      this.router.navigate(['\home']);
+      this.presentAlert('Sucesso', 'Contato cadastrado com Sucesso');
+    }
+    else{
+      this.presentAlert('Erro', 'Todos os campos são Obrigatórios');
+    }
+    this.nome = "";
+    this.telefone = "";
+  }
+
+  async presentAlert(subHeader: string, message: string) {
+    const alert = await this.alertController.create({
+      header: 'Agenda de Contatos',
+      subHeader: subHeader,
+      message: message,
+      buttons: ['OK'],
+    });
+
+    await alert.present();
+  }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/view/home/home.page.ts
+++ src/app/view/home/home.page.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { AlertController } from '@ionic/angular';
-import Contato from 'src/app/model/Contato';
+import Contato from 'src/app/model/entities/Contato';
+import { ContatoService } from 'src/app/model/services/contato.service';
 
 @Component({
   selector: 'app-home',
@@ -9,40 +11,16 @@ import Contato from 'src/app/model/Contato';
 })
 export class HomePage {
   contatos: Contato[] = [];
-  nome: string = "";
-  telefone: string = "";
-  
-  constructor(private alertController: AlertController) {
-    let c1: Contato = new Contato("Jotinha", "42999887766");
-    let c2: Contato = new Contato("Carlão", "42999881122");
-    let c3: Contato = new Contato("Gigi", "42999883344");
-    this.contatos.push(c1);
-    this.contatos.push(c2);
-    this.contatos.push(c3);
-  }
-  
-  cadastrar() {
-    if (this.nome && this.telefone){
-      let c: Contato = new Contato(this.nome, this.telefone);
-      this.contatos.push(c);
-      this.presentAlert('Sucesso', 'Contato cadastrado com Sucesso');
+
+  constructor(private router: Router, 
+    private contatoService: ContatoService) {
+      this.contatos = contatoService.obterTodos();
     }
-    else{
-      this.presentAlert('Erro', 'Todos os campos são Obrigatórios');
-    }
-    this.nome = "";
-    this.telefone = "";
+  
+  irParaCadastrar(){
+    this.router.navigate(['/cadastrar']);
   }
 
-  async presentAlert(subHeader: string, message: string) {
-    const alert = await this.alertController.create({
-      header: 'Agenda de Contatos',
-      subHeader: subHeader,
-      message: message,
-      buttons: ['OK'],
-    });
-
-    await alert.present();
-  }
+  
 
 }
